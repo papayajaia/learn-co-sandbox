c85b32de7ceb08af09ad2d1d18484481af7546c6
papayajaia <jpaginsky@gmail.com>
2018-07-23 18:43:05 +0000

    automatically backed up by learn

---
+++ MyWebsite/hello.js
@@ -0,0 +1,31 @@
+<html>
+<head>
+<link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet">
+ <h1 align="center" onmouseover="style.color='#42f4bf'" onmouseout="style.color='black'"><b>&#9829; Jaia Papaya A. &#9829;</h1></b>
+</head>
+<body>
+  <img src = "file:///Users/apple/Desktop/large.jpg" >
+  <h1>Jaia Papaya A.</h1>
+  <ul>
+  <li>Loves to play volleyball</li>
+  <li>Excited to see the world and travel to new places</li>
+  <li></li> 
+  <li></li> 
+  <li></li> 
+</ul>
+  <p><h1></h1></p>
+  <section> </section>
+  <article></article>
+  <div> </div>
+
+<ol>
+  <li> Music </li>
+  <li> Travel </li>
+  <li>this is the first list in numbers</li>
+  <li>this is the first list in numbers</li>
+</ol>
+</body>
+<foot>
+  
+</foot>
+</html>
